829288aeb3921e8246152948af02deb24f02ad27
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-05-26 08:59:22 +0200

    [bucket-test]: using all method only from module KV.Bucket

--- kv/test/kv/bucket_test.exs
+++ kv/test/kv/bucket_test.exs
@@ -2,11 +2,11 @@ defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
   test "stores values by key" do
-    {:ok, bucket} = Agent.start_link
+    {:ok, bucket} = KV.Bucket.start_link
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 10)
-    assert KV.Bucket.get(bucket, "milk") == 3
+    assert KV.Bucket.get(bucket, "milk") == 10
   end
 
 
