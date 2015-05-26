49be57df2b60e75536ae5f5ef372931ce506852c
speedcom <mateusz.maciaszekhpc@gmail.com>
2015-05-26 09:25:20 +0200

    [bucket-test]: bucket delete method test

--- kv/test/kv/bucket_test.exs
+++ kv/test/kv/bucket_test.exs
@@ -1,12 +1,23 @@
 defmodule KV.BucketTest do
   use ExUnit.Case, async: true
 
-  test "stores values by key" do
+  setup do
     {:ok, bucket} = KV.Bucket.start_link
+    {:ok, bucket: bucket}
+  end
+
+  test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 10)
     assert KV.Bucket.get(bucket, "milk") == 10
+  end
+
+  test "deletes values by key", %{bucket: bucket} do
+    assert KV.Bucket.delete(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 10)
+    assert KV.Bucket.delete(bucket, "milk") == 10
   end
 
 
